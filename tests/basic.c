d13df3fa8feb2a2ab2f6d78554fa2fa9bccc57b3
girlbuzz <minneelyyyy@proton.me>
2024-01-24 21:05:52 -0500

    add comment explaining little endian key

--- tests/basic.c
+++ tests/basic.c
@@ -22,6 +22,9 @@ void printhex(const char *data, size_t n) {
 }
 
 int main() {
+    /* this key might not work right on a big endian system.
+     * the algorithm never uses the key at a byte level, so it shouldn't be fine. However, know that internally the bytes
+     * aren't in the proper order. To export or something you will need to convert to big endian. */
     AES128Key key = { 0x2b7e1516, 0x28aed2a6, 0xabf71588, 0x09cf4f3c };
     char plaintext[] = {0x6b, 0xc1, 0xbe, 0xe2, 0x2e, 0x40, 0x9f, 0x96, 0xe9, 0x3d, 0x7e, 0x11, 0x73, 0x93, 0x17, 0x2a};
 
